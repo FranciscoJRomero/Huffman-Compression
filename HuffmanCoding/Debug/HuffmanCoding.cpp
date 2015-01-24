75aa8268e88b387d4b5e3ef0f658194aee4d67ce
Francisco <franciscoromero02@gmail.com>
2015-01-24 14:30:15 -0500

    Source code

---
+++ HuffmanCoding/Debug/HuffmanCoding.cpp
@@ -0,0 +1,29 @@
+//Francisco Romero
+//Cs 220
+//Professor: Akira Kawaguchi
+
+#include<iostream>
+#include<string>
+#include<fstream>
+#include<map>
+#include<vector>
+#include<queue>
+#include<ctime>
+#include"Huffman.h"
+
+int main(){
+
+	clock_t Begin_Time;											
+	HuffmanCoding::Huffman compressor;
+
+	Begin_Time = clock();														
+	compressor.encode();
+	std::cout << "It took\t" << float(clock() - Begin_Time) / CLOCKS_PER_SEC * 1000 << " msec to Compress the File" << std::endl;
+
+	Begin_Time = clock();														
+	compressor.decode();
+	std::cout << "It took\t" << float(clock() - Begin_Time) / CLOCKS_PER_SEC * 1000 << " msec to unCompress the File" << std::endl << std::endl;
+
+	system("pause");
+	return 0;
+}
